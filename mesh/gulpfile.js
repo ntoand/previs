7a388689df786675aee3b0c76aa8d1ecf701098e
Michael Eager <mick.eager@gmail.com>
2016-12-15 16:42:22 +1100

    Mesh previs client and server js update
    package.json includes meshes and proper url
    gulpfile.js meshviewer paths included
    server.js rename websurfer token function

--- mesh/gulpfile.js
+++ mesh/gulpfile.js
@@ -7,20 +7,36 @@ var gutil = require('gulp-util');
 var rename = require('gulp-rename');
 
 var paths = {
-	sharevol : [
-	    "public/sharemesh/lib/dat.gui.js",
-	    "public/sharemesh/lib/gl-matrix.js",
-	    "public/sharemesh/lib/OK.js",
-	    "public/sharemesh/src/slicer.js",
-	    "public/sharemesh/src/mesh.js",
-	    "public/sharemesh/src/main.js"
-	    ],
-	separate_scripts: [
-		"public/js/local.js",
-		"public/js/daris.js",
-		"public/js/tag.js"
+    meshviewer : [
+	      "public/meshviewer/lib/dat.gui.js",
+				"public/meshviewer/lib/gl-matrix.js",
+				"public/meshviewer/lib/OK.js",
+				"public/meshviewer/src/slicer.js",
+				"public/meshviewer/src/mesh.js",
+				"public/meshviewer/src/main.js"
+    ],
+	  sharevol : [
+	      "public/sharevol/lib/dat.gui.js",
+	      "public/sharevol/lib/gl-matrix.js",
+	      "public/sharevol/lib/OK.js",
+	      "public/sharevol/src/slicer.js",
+	      "public/sharevol/src/volume.js",
+	      "public/sharevol/src/main.js"
+	  ],
+	  separate_scripts: [
+		    "public/js/local.js",
+		    "public/js/daris.js",
+		    "public/js/tag.js"
 		]
 };
+
+gulp.task('scripts.sharevol', function() {
+    gulp.src(paths.sharevol)
+        .pipe(concat('sharevol.js'))
+        .pipe(gulp.dest('public/sharevol'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/sharevol'))
+});
 
 gulp.task('scripts.mesh', function() {
     gulp.src(paths.sharemesh)
