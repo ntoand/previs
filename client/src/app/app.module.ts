c38d27c32d6898b182d1090cc728ab357cfa8dda
Toan Nguyen <ntoand@gmail.com>
2017-12-26 14:55:55 +1100

    clean up

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { UploadComponent } from './components/upload/upload.component';
+import { UploadfileComponent } from './components/uploadfile/uploadfile.component';
+import { UploadlinkComponent } from './components/uploadlink/uploadlink.component';
 import { ReviewComponent } from './components/review/review.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -18,21 +20,16 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppService } from './services/app.service';
 import { WebsocketService } from './services/websocket.service';
 
-import { TitleCasePipe } from './pipes/title-case.pipe';
-import { UploadfileComponent } from './components/uploadfile/uploadfile.component';
-import { UploadlinkComponent } from './components/uploadlink/uploadlink.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UploadComponent,
-    ReviewComponent,
     HeaderComponent,
     FooterComponent,
-    TitleCasePipe,
+    UploadComponent,
     UploadfileComponent,
-    UploadlinkComponent
+    UploadlinkComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
@@ -40,13 +37,13 @@ import { UploadlinkComponent } from './components/uploadlink/uploadlink.componen
     FormsModule,
     FlexLayoutModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatInputModule,
     MatCardModule,
     MatRadioModule,
-    HttpClientModule,
     MatProgressBarModule,
     MatProgressSpinnerModule
   ],
