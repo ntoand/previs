e031571255fd25cb721845860d51c9da80fbf9b3
Toan Nguyen <ntoand@gmail.com>
2018-02-13 13:38:24 +1100

    fix bugs: PotreeConverter fails to convert PLY, link button doesn't work on Firefox

--- server/potree/build.sh
+++ server/potree/build.sh
@@ -11,6 +11,8 @@ make
 
 #build PotreeConverter
 cd ${dir}/PotreeConverter
+#master branch may have bugs when converting PLY file
+git checkout develop
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DLASZIP_INCLUDE_DIRS=${dir}/LAStools/LASzip/dll -DLASZIP_LIBRARY=${dir}/LAStools/LASzip/build/src/liblaszip.so ..
